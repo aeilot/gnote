816fb5e32e05c25e64039ebf130d92b9b894b106
Louis Aeilot <1329415023@qq.com>
2021-06-26 22:03:28 +0800

    Update arguments.h

---
+++ src/headers/arguments.h
@@ -0,0 +1,8 @@
+#ifndef ARGUMENTS_H
+#define ARGUMENTS_H
+
+struct arguementItem;
+
+class argumentProcessor;
+
+#endif
---
+++ src/source/arguments.cpp
@@ -0,0 +1,10 @@
+#include "arguments.h"
+#include <iostream>
+#include <cstdio>
+
+using namespace	std;
+
+int main(int argc, char* argv[]){
+	return 0;
+}
+
